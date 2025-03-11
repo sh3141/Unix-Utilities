fc8625334b252de2c46142c0605f4d757d6c47e6
sh3141 <158093242+sh3141@users.noreply.github.com>
2025-03-11 05:51:38 -0400

    Create pwd.c

---
+++ pwd.c
@@ -0,0 +1,23 @@
+#include <unistd.h>
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+#define SIZE 1024
+
+int pwd_main() {
+  
+    char buff[SIZE];
+    if(getcwd(buff,SIZE) ==NULL){
+        
+        return errno;
+    }
+    if(write(1,buff,strlen(buff))<0) {
+       write(2,"\033[1;31m Couldn't write the directory \033[0m \n",
+       strlen("\033[1;31m Couldn't write the directory \033[0m \n"));
+       return errno;
+    }
+
+
+    
+}
