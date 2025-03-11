9b02fc4c46220b8c14780b59b1ceccc00c791220
sh3141 <158093242+sh3141@users.noreply.github.com>
2025-03-11 05:51:06 -0400

    Create echo.c

---
+++ echo.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+
+#define LEN 100;
+int echo_main(int argc, char *argv[]) {
+    
+    if(argc == 1){
+        if(write(1,"\n",1)<0) {
+            write(2,"\033[1;31m Couldn't write the output \033[0m \n",
+            strlen("\033[1;31m Couldn't write the output \033[0m \n"));
+            return errno;
+        }
+    }
+    else{
+        if(write(1,argv[1],strlen(argv[1]))<0) {
+                write(2,"\033[1;31m Couldn't write the output \033[0m \n",
+                 strlen("\033[1;31m Couldn't write the output \033[0m \n"));
+                return errno;
+            }
+        for(int i=2;i<argc;i++){
+            if(write(1," ",1)<0) {
+                write(2,"\033[1;31m Couldn't write the output \033[0m \n",
+                 strlen("\033[1;31m Couldn't write the output \033[0m \n"));
+                return errno;
+            }
+            if(write(1,argv[i],strlen(argv[i]))<0) {
+               write(2,"\033[1;31m Couldn't write the output \033[0m \n",
+                strlen("\033[1;31m Couldn't write the output \033[0m \n"));
+               return errno;
+            }
+        }
+        if(write(1,"\n",1)<0) {
+            write(2,"\033[1;31m Couldn't write the output \033[0m \n",
+             strlen("\033[1;31m Couldn't write the output \033[0m \n"));
+            return errno;
+        }
+    }
+}
